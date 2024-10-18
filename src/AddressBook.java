4c1a9ceab0b5114470834221c4b2c61f63c108bd
Renua <renuaajayi@gmail.com>
2024-10-18 19:14:40 -0400

    last edit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,31 +1,59 @@
 import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
 
     private ArrayList<BuddyInfo> BuddyInformation;
+    private List<AddresbookView> views;
 
     public AddressBook(){
         this.BuddyInformation = new ArrayList<>();
+        views = new ArrayList<AddresbookView>();
 
+    }
+    public void addAddressBookView(AddresbookView view){
+        this.views.add(view);
     }
 
     public void addBuddy(BuddyInfo info){
         BuddyInformation.add(info);
+        handleviewupdate();
     }
 
     public void removeBuddy(BuddyInfo info){
         BuddyInformation.remove(info);
+
+        handleviewupdate();
     }
 
     public void friendsprint(int i){
        System.out.println(" what i want to see " +  BuddyInformation.get(i));
 
+
+    }
+    public List<BuddyInfo> getBuddies() {
+        return BuddyInformation;
     }
 
-    public static void main(String[] args) {
+    private void handleviewupdate(){
+        for (AddresbookView view : views) {
+            view.handleviewupdate();
+        }
+    }
+
+
+    public void seeBuddys(){
+        for(BuddyInfo buddiesInfo: BuddyInformation){
+            System.out.println("my name is " + buddiesInfo.name() + " i live at " + buddiesInfo.Address() + " my phone number is " + buddiesInfo.number());
+        }
+    }
+
+   /* public static void main(String[] args) {
         System.out.println("address book");
         BuddyInfo Renua = new BuddyInfo("Daniel","12 street","445678" );
         BuddyInfo Tola = new BuddyInfo("TOla","14 street","422678" );
         AddressBook friendsaddress = new AddressBook();// new
+        friendsaddress.seeBuddys();
 
-    }
+    }*/
 }
